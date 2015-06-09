027d87341f1460595088aef5130526afd5de5527
Emerald Guo <emerald.guo@morningstar.com>
2015-06-09 16:26:37 +0800

    Add code for spider

--- test/zy/zy/spiders/zy_spider.py
+++ test/zy/zy/spiders/zy_spider.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class HouseTrade():
+    garden_name =''
+    garden_price = ''
+    trade_date = ''
+    garden_sumPrice = ''
+
+    def getInfo(self):
+        return self.garden_name + "," + self.garden_price +"," + self.trade_date + "," + self.garden_sumPrice + ","
 
 
 class ZySpiderSpider(scrapy.Spider):
@@ -9,11 +22,52 @@ class ZySpiderSpider(scrapy.Spider):
         'http://sz.centanet.com/chengjiao/',
     )
 
+    def getnextpage(self, response):
+        for buttom_data in response.xpath('//div[@class="bottom-paging"]/div[@class="zjfy"]'):
+            nextpage = buttom_data.xpath('//div[@class="fy2"]/a/@href').extract()[-1]
+            return nextpage
+        
+    def getnextpage_text(self, response):
+        for buttom_data in response.xpath('//div[@class="bottom-paging"]/div[@class="zjfy"]'):
+            nextpage = buttom_data.xpath('//div[@class="fy2"]/a/text()').extract()[-1]
+            return nextpage
+
     def parse(self, response):
-        with open('zy_data', 'wb') as f:
+        with open('zy_data.csv', 'a+') as f:
             # f.write(response.body)
+            #for buttom_data in response.xpath('//div[@class="bottom-paging"]/div[@class="zjfy"]'):
+            #    nextpage = buttom_data.xpath('//div[@class="fy2"]/a/@href').extract()[0]
+            #    print nextpage
+                
             for one_cj_data in response.xpath('//div[@class="cj_fy_box"]/ul/li'):
-                garden_name = one_cj_data.xpath('.//div[@class="cjfy_xx esfbt"]/node()[last()]').extract()[0]
-                print garden_name
-                f.write(garden_name)
+                houseTrade = HouseTrade();
+                houseTrade.garden_name = one_cj_data.xpath('.//div[@class="cjfy_xx esfbt"]/node()[last()]').extract()[0]
+                houseTrade.garden_price = one_cj_data.xpath('.//div[@class="time_zy"]/span[@class="red22"]/node()').extract()[0]
+                houseTrade.trade_date = one_cj_data.xpath('.//div[@class="time_zy"]/span[@class="black24"]/node()').extract()[0]
+                houseTrade.garden_sumPrice = one_cj_data.xpath('.//div[@class="zj_zy"]/span[@class="red22"]/node()').extract()[0]
+
+                houseTrade.garden_price = houseTrade.garden_price.strip("元/平")
+                houseTrade.garden_sumPrice = houseTrade.garden_sumPrice.strip("万")
+                
+                
+                print houseTrade.garden_name
+                print houseTrade.garden_price
+                print houseTrade.trade_date
+                print houseTrade.garden_sumPrice              
+                f.write(houseTrade.getInfo())
+            f.close()
+
+            nextpageText = self.getnextpage_text(response)
+            print nextpageText
+
+            if nextpageText == "末页":
+                print "Search next pages"
+                nextpage = self.getnextpage(response)
+                next_urls = 'http://sz.centanet.com' + nextpage
+                yield scrapy.Request(next_urls, callback=self.parse)
+            
+            
+                
+
+    
 
